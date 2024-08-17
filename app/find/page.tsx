18071426417137001e9f43e95a5788ce051eb570
Bailey Frye <baileyafrye@comcast.net>
2024-08-16 19:50:16 -0500

    Added market grid to Find Us page

--- app/find/page.tsx
+++ app/find/page.tsx
@@ -1,4 +1,30 @@
+import Container from '@/components/global/Container';
+import markets from '@/utils/markets';
+import Market from '@/components/Market';
+
 const FindUsPage = () => {
-  return <div>FindUsPage</div>;
+  return (
+    <section className='bg-primary-400 py-12'>
+      <Container>
+        <div></div>
+        <div className='grid lg:grid-cols-3 gap-8'>
+          {markets.map((market) => {
+            const { id, marketName, dayAndTime, availability, location, url } =
+              market;
+            return (
+              <Market
+                key={id}
+                marketName={marketName}
+                dayAndTime={dayAndTime}
+                availability={availability}
+                location={location}
+                url={url}
+              />
+            );
+          })}
+        </div>
+      </Container>
+    </section>
+  );
 };
 export default FindUsPage;
