499126a3ce330baa562d046ba524f1c9e5d9c5e2
baileyfrye1 <baileyafrye@comcast.net>
2025-01-03 15:42:51 -0600

    Updated pricing of knives and added more knives to the pricing list

--- utils/knives.ts
+++ utils/knives.ts
@@ -4,17 +4,17 @@ const knives = [
   {
     id: uuid(),
     knifeType: 'Ceramic Knives',
-    pricing: '$9 - $15 (based on blade length)',
+    pricing: '$45/hr',
   },
   {
     id: uuid(),
     knifeType: 'Meat Cleaver',
-    pricing: '$9',
+    pricing: '$10',
   },
   {
     id: uuid(),
     knifeType: 'Kitchen Knife (blade 1 to 6 in.)',
-    pricing: '$7 smooth edge / $8 serrated edge',
+    pricing: '$8 smooth edge / $9 serrated edge',
   },
   {
     id: uuid(),
@@ -29,7 +29,7 @@ const knives = [
   {
     id: uuid(),
     knifeType: 'Hunting / Tactical Knife (blade 1 to 6 in.)',
-    pricing: '$7 smooth edge / $8 serrated edge',
+    pricing: '$8 smooth edge / $9 serrated edge',
   },
   {
     id: uuid(),
@@ -45,6 +45,68 @@ const knives = [
     id: uuid(),
     knifeType: 'Knife Tip, Bolster, Heel, or Bevel Repair',
     pricing: '(if possible) - $5',
+  },
+  {
+    id: uuid(),
+    knifeType: 'Axe and Hatchet',
+    pricing: '$10 (Large and numerous chips carry additional charge)',
+  },
+  {
+    id: uuid(),
+    knifeType: 'Machete (up to 20 in.)',
+    pricing: '$15',
+  },
+  {
+    id: uuid(),
+    knifeType: 'Pizza Cutter',
+    pricing: '$6',
+  },
+  {
+    id: uuid(),
+    knifeType: 'Western Chisel and Plane Blade (straight edge up to 3 in.)',
+    pricing: '$11',
+  },
+  {
+    id: uuid(),
+    knifeType: 'Turning and Carving Tools (No V-Tools or carbide tips)',
+    pricing: '$12',
+  },
+  {
+    id: uuid(),
+    knifeType: 'Rotary Mower Blade (clean and sharpen)',
+    pricing: '$10',
+  },
+  {
+    id: uuid(),
+    knifeType:
+      'Hand Pruner / Hedge Trimmer or Loppers (heavy rust removal / clean up additional charge)',
+    pricing: '$11 / $5',
+  },
+  {
+    id: uuid(),
+    knifeType: 'Electric / Gas Trimmers',
+    pricing: '$18',
+  },
+  {
+    id: uuid(),
+    knifeType: 'Edges, Hoes, and Shovels',
+    pricing: '$8',
+  },
+  {
+    id: uuid(),
+    knifeType:
+      'Chainsaw Chains (including cleaning and depth gauge when needed / up to 25 in.)',
+    pricing: '$10',
+  },
+  {
+    id: uuid(),
+    knifeType: 'Household and Fabric Scissors (up to 12 in.)',
+    pricing: '$7',
+  },
+  {
+    id: uuid(),
+    knifeType: 'Pinking Shears (any size)',
+    pricing: '$8',
   },
 ];
 
