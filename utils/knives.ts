0498a36ea171dbbd6d0727a942090a30e3b5473e
baileyfrye1 <baileyafrye@comcast.net>
2025-01-03 17:18:48 -0600

    Removed new knives added, kept new pricing

--- utils/knives.ts
+++ utils/knives.ts
@@ -46,68 +46,6 @@ const knives = [
     knifeType: 'Knife Tip, Bolster, Heel, or Bevel Repair',
     pricing: '(if possible) - $5',
   },
-  {
-    id: uuid(),
-    knifeType: 'Axe and Hatchet',
-    pricing: '$10 (Large and numerous chips carry additional charge)',
-  },
-  {
-    id: uuid(),
-    knifeType: 'Machete (up to 20 in.)',
-    pricing: '$15',
-  },
-  {
-    id: uuid(),
-    knifeType: 'Pizza Cutter',
-    pricing: '$6',
-  },
-  {
-    id: uuid(),
-    knifeType: 'Western Chisel and Plane Blade (straight edge up to 3 in.)',
-    pricing: '$11',
-  },
-  {
-    id: uuid(),
-    knifeType: 'Turning and Carving Tools (No V-Tools or carbide tips)',
-    pricing: '$12',
-  },
-  {
-    id: uuid(),
-    knifeType: 'Rotary Mower Blade (clean and sharpen)',
-    pricing: '$10',
-  },
-  {
-    id: uuid(),
-    knifeType:
-      'Hand Pruner / Hedge Trimmer or Loppers (heavy rust removal / clean up additional charge)',
-    pricing: '$11 / $5',
-  },
-  {
-    id: uuid(),
-    knifeType: 'Electric / Gas Trimmers',
-    pricing: '$18',
-  },
-  {
-    id: uuid(),
-    knifeType: 'Edges, Hoes, and Shovels',
-    pricing: '$8',
-  },
-  {
-    id: uuid(),
-    knifeType:
-      'Chainsaw Chains (including cleaning and depth gauge when needed / up to 25 in.)',
-    pricing: '$10',
-  },
-  {
-    id: uuid(),
-    knifeType: 'Household and Fabric Scissors (up to 12 in.)',
-    pricing: '$7',
-  },
-  {
-    id: uuid(),
-    knifeType: 'Pinking Shears (any size)',
-    pricing: '$8',
-  },
 ];
 
 export default knives;
